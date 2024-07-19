de8e56e7df3caf7275d91af7b7c3ead66697beab
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-07-19 12:07:48 +0200

    fonction speed

--- cours_similator.py
+++ cours_similator.py
@@ -13,3 +13,36 @@ max_number_horses =20
 
 def start_dice():
     return  random.randint(1, 6)
+ def Speed(currenct_speed, DQ):
+     if DQ:
+         return currenct_speed, DQ
+     dice=start_dice()
+     if currenct_speed ==0:
+         if dice ==1 or dice==2:
+             DQ= True
+     elif currenct_speed ==1:
+         if dice==1:
+             currenct_speed +=1
+     elif currenct_speed==2:
+         if dice in [1,2]:
+             currenct_speed +=1
+     elif currenct_speed==3:
+         if dice in [4,5]:
+             currenct_speed +=1
+         elif dice==1:
+             currenct_speed -=1
+     elif currenct_speed==4:
+         if dice==1:
+             currenct_speed -=1
+     elif currenct_speed==5:
+         if dice in [1,2]:
+             currenct_speed -=1
+     elif currenct_speed ==6:
+         if dice in [1,2]:
+             currenct_speed -=1
+         elif dice==6:
+             DQ=True
+
+     if currenct_speed<0:
+         currenct_speed
+
